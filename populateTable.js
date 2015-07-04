73cea79a13165cb9e0d64f9c2da2f821219abb0c
selemon <selemon3yitbarek@hotmail.com>
2015-07-04 15:10:54 +1200

    creting table

--- populateTable.js
+++ populateTable.js
@@ -2,11 +2,29 @@
  * Created by selemon on 4/07/15.
  */
 var pg = require('pg').native
+    , d3 = require("d3")
     , connectionString = process.env.DATABASE_URL
     , client
     , query;
 
 client = new pg.Client(connectionString);
 client.connect();
-query = client.query("COPY data FROM 'CrimeDataNzCurrent.txt'");
+
+var qu;
+var o = 'INSERT INTO data (Event_ID ,Code ,Event_Type ,Category ,Status ,Location ,Suburb ,City ,Published) ';
+function parse(){
+    d3.csv("CrimeDataNzCurrent.csv", function(error, data) {
+        qu = data;
+        qu.forEach(function(d){
+            o+= 'VALUES ($$'+ d.Event_ID+'$$, $$'+ d.Code+'$$, $$'+ d.Event_Type+'$$, $$'+ d.Category+'$$, $$'+ d.Status+'$$, $$'+ d.Location+'$$, $$'+ d.Suburb+'$$, $$'+ d.City+'$$, $$'+ d.Published+'$$); ';
+            o+= 'INSERT INTO data (Event_ID ,Code ,Event_Type ,Category ,Status ,Location ,Suburb ,City ,Published) ';
+
+        });
+
+    });
+}
+
+
+
+query = client.query(o);
 query.on('end', function() { client.end(); });
