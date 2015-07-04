38705fbba4dc9865fecf3f04312b3b12a9809d0b
selemon <selemon3yitbarek@hotmail.com>
2015-07-04 15:15:40 +1200

    creting table

--- populateTable.js
+++ populateTable.js
@@ -11,13 +11,14 @@ client = new pg.Client(connectionString);
 client.connect();
 
 var qu;
-var o = 'INSERT INTO data (Event_ID ,Code ,Event_Type ,Category ,Status ,Location ,Suburb ,City ,Published) ';
+var o;
 function parse(){
     d3.csv("CrimeDataNzCurrent.csv", function(error, data) {
         qu = data;
+        o = 'INSERT INTO data (Event_ID, Code, Event_Type, Category, Status, Location, Suburb, City, Published)';
         qu.forEach(function(d){
-            o+= 'VALUES ($$'+ d.Event_ID+'$$, $$'+ d.Code+'$$, $$'+ d.Event_Type+'$$, $$'+ d.Category+'$$, $$'+ d.Status+'$$, $$'+ d.Location+'$$, $$'+ d.Suburb+'$$, $$'+ d.City+'$$, $$'+ d.Published+'$$); ';
-            o+= 'INSERT INTO data (Event_ID ,Code ,Event_Type ,Category ,Status ,Location ,Suburb ,City ,Published) ';
+            o+= ' VALUES ($$'+ d.Event_ID+'$$, $$'+ d.Code+'$$, $$'+ d.Event_Type+'$$, $$'+ d.Category+'$$, $$'+ d.Status+'$$, $$'+ d.Location+'$$, $$'+ d.Suburb+'$$, $$'+ d.City+'$$, $$'+ d.Published+'$$);';
+            o+= ' INSERT INTO data (Event_ID, Code, Event_Type, Category, Status, Location, Suburb, City, Published)';
 
         });
 
