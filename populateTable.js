a7b9d679c934575c593e562c6cb6ef623a5f7b55
selemon <selemon3yitbarek@hotmail.com>
2015-07-04 13:13:04 +1200

    creting table

--- populateTable.js
+++ populateTable.js
@@ -8,5 +8,5 @@ var pg = require('pg').native
 
 client = new pg.Client(connectionString);
 client.connect();
-query = client.query("COPY data FROM $1 DELIMITER $2 CSV", ['CrimeDataNzCurrent.csv', ',']);
+query = client.query("COPY data FROM 'CrimeDataNzCurrent.csv' DELIMITER ',' CSV");
 query.on('end', function() { client.end(); });
