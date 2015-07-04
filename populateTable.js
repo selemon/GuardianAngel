dad09ace426d1b4de215384a1278e0f61dc2817e
selemon <selemon3yitbarek@hotmail.com>
2015-07-04 15:44:36 +1200

    creting table

--- populateTable.js
+++ populateTable.js
@@ -2,7 +2,6 @@
  * Created by selemon on 4/07/15.
  */
 var pg = require('pg').native
-    , d3 = require("d3")
     , connectionString = process.env.DATABASE_URL
     , client
     , query;
@@ -10,22 +9,5 @@ var pg = require('pg').native
 client = new pg.Client(connectionString);
 client.connect();
 
-var qu;
-var o;
-function parse(){
-    d3.csv("CrimeDataNzCurrent.csv", function(error, data) {
-        qu = data;
-        o = 'INSERT INTO data (Event_ID, Code, Event_Type, Category, Status, Location, Suburb, City, Published)';
-        qu.forEach(function(d){
-            o+= ' VALUES ($$'+ d.Event_ID+'$$, $$'+ d.Code+'$$, $$'+ d.Event_Type+'$$, $$'+ d.Category+'$$, $$'+ d.Status+'$$, $$'+ d.Location+'$$, $$'+ d.Suburb+'$$, $$'+ d.City+'$$, $$'+ d.Published+'$$);';
-            o+= ' INSERT INTO data (Event_ID, Code, Event_Type, Category, Status, Location, Suburb, City, Published)';
-
-        });
-
-    });
-}
-
-parse();
-
-query = client.query(o);
+query = client.query();
 query.on('end', function() { client.end(); });
--- server.js
+++ server.js
@@ -66,7 +66,47 @@ app.get('/getData', function(req, res) {
 
 });
 
+app.post('/PostData', function(req, res) {
 
+
+
+            // Grab elements from body JSON object.
+            title = req.body.data;
+
+            // Basic sanity checking. Is this even going to work?
+            if(	title === undefined){
+                res.status(400).send("Bad request. Some parameter not defined or missing. I recieved this body:" + req.body);
+                return;
+            }
+            //Connect to database.
+            pg.connect(connectionString, function (err, client, done) {
+                console.log("DB Connected with port: " + port);
+                // SQL parameterized query to insert entry.
+                query = client.query(title);
+
+                var results = [];  //stores results.. in this case should just be one.
+                //handle database events. errors first.
+                query.on('error', function (errorMsg) {
+                    console.log("Debug: database error:" + errorMsg);
+                    res.status(400).send({"error": "database error"});
+                    client.end();
+                });
+                // Stream results back one row at a time
+                query.on('row', function (resultRow) {
+                    console.log("Debug: (/memory/store) returning obejct: " + resultRow);
+                    results.push(resultRow);
+                });
+                // After all data is returned, close connection and return results
+                query.on('end', function () {
+                    client.end();
+                    res.status(200).send({"hello": "it worked"});
+                });
+                // Handle Errors - by console display
+                if (err) {	console.log(err);  }
+            });
+
+
+});
 
 
 // use PORT set as an environment variable
