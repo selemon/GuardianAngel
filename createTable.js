9b3e4c49713d9aa9ecf01b75dc70dae5190724fb
selemon <selemon3yitbarek@hotmail.com>
2015-07-04 13:01:44 +1200

    creting table

--- createTable.js
+++ createTable.js
@@ -8,5 +8,5 @@ var pg = require('pg').native
 
 client = new pg.Client(connectionString);
 client.connect();
-query = client.query('CREATE TABLE quotes (quote_id BIGSERIAL PRIMARY KEY, author varchar(255) UNIQUE, quote varchar(255) NOT NULL)');
+query = client.query('CREATE TABLE data (Event_ID varchar(55) PRIMARY KEY, Code varchar(55), Event_Type varchar(255), Category varchar(255), Status varchar(255), Location varchar(255), Suburb varchar(255), City varchar(255), Published date)');
 query.on('end', function() { client.end(); });
